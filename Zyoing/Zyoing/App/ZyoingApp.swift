f5b999e62a113ed5d1728c86eb05a8acc820eda3
SK <2mailskk@gmail.com>
2023-07-07 12:59:53 +0530

    initialise project with directory setups

---
+++ Zyoing/Zyoing/App/ZyoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZyoingApp.swift
+//  Zyoing
+//
+//  Created by Krishna on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ZyoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Zyoing/Zyoing/Core/View/HomeView/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Zyoing
+//
+//  Created by Krishna on 07/07/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
